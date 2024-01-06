8535e585c72a8f5c6a5f0f54f302bb5050af5b47
Roland Bewick <roland.bewick@gmail.com>
2024-01-06 18:20:07 +0700

    fix: remove unused import

--- src/pages/wallet/Share.tsx
+++ src/pages/wallet/Share.tsx
@@ -1,6 +1,5 @@
 import QRCode from "qrcode.react";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { Backbar } from "../../components/Backbar";
 
 export function Share() {
