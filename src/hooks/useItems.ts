9b45461f6a99152cac781767d6bc61bc94793016
Sepehr Safari <safari.sepehr@gmail.com>
2024-01-15 12:13:19 +0330

    refactor useItems and implement ndk

--- src/hooks/useItems.ts
+++ src/hooks/useItems.ts
@@ -1,28 +1,46 @@
-import { useSubscribe } from 'nostr-hooks';
-import React from 'react';
-import { RELAYS, appCustomDataTag, appCustomDataValues } from '../constants';
+import { NDKEvent, NDKFilter } from '@nostr-dev-kit/ndk';
+import { useEffect, useMemo, useState } from 'react';
+
+import { appCustomDataTag, appCustomDataValues } from '../constants';
+import useStore from '../state/store';
 import { Item } from '../types';
 
-export function useItems(walletPubkey?: string) {
-  const { events, invalidate, eose } = useSubscribe({
-    relays: RELAYS,
-    filters: walletPubkey
-      ? [
-          {
-            authors: [walletPubkey],
-            kinds: [30078],
-            limit: 100,
-            // custom hashtag filter support is not supported by all relays?
-            // [`#${appCustomDataTag}`]: [appCustomDataValues.item],
-          },
-        ]
-      : [],
-    options: {
-      enabled: !!walletPubkey,
-    },
-  });
+export function useItems() {
+  const [events, setEvents] = useState<NDKEvent[]>([]);
+  const [eose, setEose] = useState<boolean>(false);
 
-  const items = React.useMemo(
+  const ndk = useStore((store) => store.ndk);
+  const walletPubkey = useStore((store) => store.provider)?.publicKey;
+
+  useEffect(() => {
+    if (!walletPubkey) {
+      return;
+    }
+
+    const filters: NDKFilter[] = [
+      {
+        authors: [walletPubkey],
+        kinds: [30078],
+        limit: 100,
+        // custom hashtag filter support is not supported by all relays?
+        // [`#${appCustomDataTag}`]: [appCustomDataValues.item],
+      },
+    ];
+
+    setEose(false);
+
+    const subscription = ndk.subscribe(filters);
+
+    subscription.on('event', (event) => {
+      setEvents((prevEvents) => [...prevEvents, event]);
+    });
+
+    subscription.on('eose', () => {
+      setEose(true);
+    });
+  }, [ndk, walletPubkey, setEvents]);
+
+  const items = useMemo(
     () =>
       // TODO: remove filter when above relay filter works
       events
@@ -33,8 +51,8 @@ export function useItems(walletPubkey?: string) {
     [events]
   );
 
-  const isLoading = !eose && !events.length;
+  const isLoading = !eose && events.length == 0;
   // console.log(items, events);
 
-  return { items, invalidate, isLoading };
+  return { items, isLoading };
 }
