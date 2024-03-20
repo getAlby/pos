cb26bfc401d4c3bc7e02d95c9490a0f220deea93
René Aaron <rene@twentyuno.net>
2024-03-20 09:10:22 +0100

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ const pwaConfig: Partial<VitePWAOptions> = {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA(pwaConfig)],
-  base: "/pos/",
+  base: "/",
   server: {
     host: "0.0.0.0",
   },
